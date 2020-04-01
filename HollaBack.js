f63a1d53f213dda21d898f4edcc037643366254e
DKleinot <63006961+DKleinot@users.noreply.github.com>
2020-04-01 15:06:42 -0400

    Update HollaBack.js

--- HollaBack.js
+++ HollaBack.js
@@ -4,6 +4,15 @@
 // Date:		04/01/2020
 // Purpose:		This will handle the call back from authentication.
 
+
+//This will make it easier to turn off debugging
+var bDebug = true;
+
+function log(Val) {
+    //Simple function to help log stuff to the console.
+    if (bDebug) { console.log(Val); }
+}
+
 function AutoLoad() {
-    log("Hey hey hey!");
+    log("Hey hey hey?");
 }
