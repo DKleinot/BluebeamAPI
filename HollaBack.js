fb18f8080ed23e29b341fa00b014159400e22d7a
DKleinot <63006961+DKleinot@users.noreply.github.com>
2020-04-01 15:16:14 -0400

    Update HollaBack.js

--- HollaBack.js
+++ HollaBack.js
@@ -15,4 +15,27 @@ function log(Val) {
 
 function AutoLoad() {
     log("Hey hey hey?");
+
+    var parser = document.createElement('a'), searchObject = {}, queries, split, i;
+
+    // Let the browser do the work
+
+    parser.href = window.location.href;
+
+    // Convert query string to object
+    queries = parser.search.replace(/^\?/, '').split('&');
+    for (i = 0; i < queries.length; i++) {
+        split = queries[i].split('=');
+        searchObject[split[0]] = split[1];
+    }
+    log(
+        parser.protocol,
+        parser.host,
+        parser.hostname,
+        parser.port,
+        parser.pathname,
+        parser.search,
+        searchObject,
+        parser.hash
+    );
 }
