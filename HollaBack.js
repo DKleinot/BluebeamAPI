4bdf0b2ec72bc93baf9ea09005bb8f7e63f410c9
DKleinot <63006961+DKleinot@users.noreply.github.com>
2020-04-01 14:58:30 -0400

    none

---
+++ HollaBack.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+// Title:		HollaBack
+// Created by:	David Kleinot
+// Date:		04/01/2020
+// Purpose:		This will handle the call back from authentication.
+
+function AutoLoad() {
+    log("Hey hey hey!");
+}
