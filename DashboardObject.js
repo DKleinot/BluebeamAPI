f486fa0d79b0dd9c3f6a00d6b242bf7232903de2
DKleinot <63006961+DKleinot@users.noreply.github.com>
2020-04-06 13:14:26 -0400

    Update DashboardObject.js

--- DashboardObject.js
+++ DashboardObject.js
@@ -35,9 +35,9 @@ function Main() {
 
     //Sessions[0].Users[0] = new SessionStruct().UsersStruct();
 
-    Sessions[0].addUser();
-    Sessions[0].Users[0].ID = "123456789";
-    Sessions[0].Users[0].Email = "D@d.com";
+    //Sessions[0].addUser();
+    //Sessions[0].Users[0].ID = "123456789";
+    //Sessions[0].Users[0].Email = "D@d.com";
 
     log(Sessions[0]);
 }
