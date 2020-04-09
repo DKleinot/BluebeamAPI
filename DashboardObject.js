3e56dfb17550cfabf4f2dfb8fd89d432d7b2244f
DKleinot <63006961+DKleinot@users.noreply.github.com>
2020-04-09 10:49:53 -0400

    Update DashboardObject.js

--- DashboardObject.js
+++ DashboardObject.js
@@ -150,7 +150,8 @@ function AddSessionToUser(uName, sID) {
                 //log(Users[getUserIndex(uName)].mySessions);
                 for (i = 0; i < iMySessions.length; i++) {
 
-                    log("Iterating on " + iMySessions[i].ID + ": current is " + sID + " == " + iMySessions[i].ID == sID);
+                    log("Iterating on " + iMySessions[i].ID + ": current is " + sID);
+                    log(iMySessions[i].ID == sID);
                     if (iMySessions[i].ID = sID) {
                         log("already has it, do nothing");
                     } else {
